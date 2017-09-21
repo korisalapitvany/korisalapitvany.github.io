83018931c36057a5f455eef90e8c65b8d2ab0a89
Attila Oláh <atl@google.com>
2017-09-21 20:42:35 +0200

    Preserve location.search when redirecting to the canonical URL.

--- _includes/js/canonical.js
+++
@@ -1,9 +0,0 @@
-(() => {
-'use strict';
-
-const canonical = document.head.querySelector('link[rel=canonical]').href + location.hash;
-if (location.href !== canonical && canonical) {
-  location.href = canonical;
-}
-
-})();
---
+++ js/canonical.coffee
@@ -0,0 +1,6 @@
+---
+---
+
+canonical = document.head.querySelector('link[rel=canonical]').href + location.search + location.hash
+
+location.href = canonical if location.href isnt canonical and canonical
