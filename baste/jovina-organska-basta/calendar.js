d320948a9b2efdb1f9f7bb0e5d4a12e090904fd9
Attila Oláh <atl@google.com>
2019-10-05 14:27:31 +0200

    HTML calendar tooltips.

--- baste/jovina-organska-basta/calendar.js
+++ baste/jovina-organska-basta/calendar.js
@@ -1,26 +1,37 @@
 "use strict";
 
+const longMonths = [
+  "januar", "februar", "mart", "april", "maj", "jun",
+  "jul", "avgust", "septembar", "oktobar", "novembar", "decembar",
+];
+
+const shortMonths = longMonths.map((month) => month.substr(0, 3));
+
 google.charts.setOnLoadCallback(() => {
   const dataTable = new google.visualization.DataTable();
   dataTable.addColumn({type: "date", id: "date" });
   dataTable.addColumn({type: "number", id: "value" });
-  dataTable.addColumn({type: "string", role: "tooltip"});
+  dataTable.addColumn({type: "string", role: "tooltip", p: {html: true}});
   dataTable.addRows([
     [new Date(2018, 6, 2), -1, "Rezultati konkursa"],
     [new Date(2018, 6, 18), -1, "Potpis ugovora"],
     [new Date(2018, 6, 23), -1, "Prenos sredstava"],
     [new Date(2019, 4, 31), 1, "Rok za slanje dnevnika aktivnosti"],
-  ]);
+  ].map((row) => [
+    row[0],
+    row[1],
+    tooltip(row[0], row[2]),
+  ]));
   const submit = new Date(2018, 4, 31);
   dateRange(new Date(2018, 4, 3), new Date(2018, 5, 12)).forEach((date) => {
     if (date.getTime() === submit.getTime()) {
-      dataTable.addRow([date, -1, "Prijava na konkurs"]);
+      dataTable.addRow([date, -1, tooltip(date, "Prijava na konkurs")]);
     } else {
-      dataTable.addRow([date, -0.25, "Otvoren konkurs"]);
+      dataTable.addRow([date, -0.25, tooltip(date, "Otvoren konkurs")]);
     }
   });
   dateRange(new Date(2018, 8, 1), new Date(2019, 4, 15)).forEach((date) => {
-    dataTable.addRow([date, 0.75, "Realizacija projekta"]);
+    dataTable.addRow([date, 0.75, tooltip(date, "Realizacija projekta")]);
   });
 
   const div = document.getElementById("calendar");
@@ -40,11 +51,8 @@ google.charts.setOnLoadCallback(() => {
        node.remove();
      });
      // Translate months:
-     [
-       "jan", "feb", "mar", "apr", "maj", "jun",
-       "jul", "avg", "sep", "okt", "nov", "dec",
-     ].forEach((month, i) => {
-       textNodes[i + 4].innerHTML = month;
+     shortMonths.forEach((month, i) => {
+       textNodes[i + 4].innerHTML = month.toUpperCase();
      });
   });
 
@@ -59,7 +67,7 @@ google.charts.setOnLoadCallback(() => {
       },
     },
     tooltip: {
-      isHtml: false,
+      isHtml: true,
     },
     height: 320,
     width: 920,
@@ -73,4 +81,28 @@ function dateRange(start, stop) {
     result.push(new Date(start));
   }
   return result;
+}
+
+function tooltip(date, text) {
+  const div = document.createElement("div");
+  div.className = "tooltip";
+
+  const strong = document.createElement("strong");
+  strong.innerText = text;
+  div.appendChild(strong);
+
+  const br = document.createElement("br");
+  div.appendChild(br);
+
+  const em = document.createElement("em");
+  em.innerText = `${
+    date.getDate()
+  }. ${
+    longMonths[date.getMonth()]
+  } ${
+    date.getFullYear()
+  }.`;
+  div.appendChild(em);
+
+  return div.outerHTML;
 }
