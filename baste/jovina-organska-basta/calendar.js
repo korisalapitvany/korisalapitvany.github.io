1194a869800190542d636f05fdf40a6abc76903c
Attila Oláh <atl@google.com>
2019-10-06 15:14:18 +0200

    Extract out calendar functionality.

--- baste/jovina-organska-basta/calendar.js
+++ baste/jovina-organska-basta/calendar.js
@@ -1,112 +1,16 @@
 "use strict";
 
-(() => {
-
-const longMonths = [
-  "januar", "februar", "mart", "april", "maj", "jun",
-  "jul", "avgust", "septembar", "oktobar", "novembar", "decembar",
-];
-
-const shortMonths = longMonths.map((month) => month.substr(0, 3));
-
-google.charts.setOnLoadCallback(() => {
-  const dataTable = new google.visualization.DataTable();
-  dataTable.addColumn({type: "date", id: "date" });
-  dataTable.addColumn({type: "number", id: "value" });
-  dataTable.addColumn({type: "string", role: "tooltip", p: {html: true}});
-  dataTable.addRows([
-    [new Date(2018, 6, 2), -1, "Rezultati konkursa"],
-    [new Date(2018, 6, 18), -1, "Potpis ugovora"],
-    [new Date(2018, 6, 23), -1, "Prenos sredstava"],
-    [new Date(2019, 4, 31), 1, "Rok za slanje dnevnika aktivnosti"],
-  ].map((row) => [
-    row[0],
-    row[1],
-    tooltip(row[0], row[2]),
-  ]));
-  const submit = new Date(2018, 4, 31);
-  dateRange(new Date(2018, 4, 3), new Date(2018, 5, 12)).forEach((date) => {
-    if (date.getTime() === submit.getTime()) {
-      dataTable.addRow([date, -1, tooltip(date, "Prijava na konkurs")]);
-    } else {
-      dataTable.addRow([date, -0.25, tooltip(date, "Otvoren konkurs")]);
-    }
-  });
-  dateRange(new Date(2018, 8, 1), new Date(2019, 4, 15)).forEach((date) => {
-    dataTable.addRow([date, 0.75, tooltip(date, "Realizacija projekta")]);
-  });
-
-  const div = document.getElementById("calendar");
-  const chart = new google.visualization.Calendar(div);
-
-   google.visualization.events.addListener(chart, "ready", () => {
-     const textNodes = div.querySelectorAll("text");
-     // Remove legend: [low, mid, high] values.
-     textNodes[0].remove();
-     textNodes[1].remove();
-     textNodes[2].remove();
-     // Remove legend box and gradient:
-     div.querySelectorAll("[fill-opacity='1']").forEach((node) => {
-       node.remove();
-     });
-     div.querySelectorAll("linearGradient").forEach((node) => {
-       node.remove();
-     });
-     // Translate months:
-     shortMonths.forEach((month, i) => {
-       textNodes[i + 4].innerHTML = month.toUpperCase();
-     });
-  });
-
-  chart.draw(dataTable, {
-    legend: "none",
-    calendar: {
-      cellSize: 16,
-      daysOfWeek: "NPUSČPS",
-      monthLabel: {
-        fontName: "'Raleway', 'Verdana', 'Arial', sans-serif",
-        fontSize: 14,
-      },
-    },
-    tooltip: {
-      isHtml: true,
-    },
-    height: 320,
-    width: 920,
-  });
+CHARTS.calendar({
+  id: "calendar",
+  data: [
+    [[new Date(2018, 4,  3),
+      new Date(2018, 5, 12)], -0.25, "Otvoren konkurs"],
+    [[new Date(2018, 4, 31)], -1,    "Prijava na konkurs"],
+    [[new Date(2018, 6, 2)],  -1,    "Rezultati konkursa"],
+    [[new Date(2018, 6, 18)], -1,    "Potpis ugovora"],
+    [[new Date(2018, 6, 23)], -1,    "Prenos sredstava"],
+    [[new Date(2019, 4, 31)],  1,    "Rok za slanje dnevnika aktivnosti"],
+    [[new Date(2018, 8, 1),
+      new Date(2019, 4, 15)],  0.75, "Realizacija projekta"],
+  ],
 });
-
-function dateRange(start, stop) {
-  const result = [new Date(start)];
-  while (start < stop) {
-    start.setDate(start.getDate() + 1);
-    result.push(new Date(start));
-  }
-  return result;
-}
-
-function tooltip(date, text) {
-  const div = document.createElement("div");
-  div.className = "tooltip";
-
-  const strong = document.createElement("strong");
-  strong.innerText = text;
-  div.appendChild(strong);
-
-  const br = document.createElement("br");
-  div.appendChild(br);
-
-  const em = document.createElement("em");
-  em.innerText = `${
-    date.getDate()
-  }. ${
-    longMonths[date.getMonth()]
-  } ${
-    date.getFullYear()
-  }.`;
-  div.appendChild(em);
-
-  return div.outerHTML;
-}
-
-})();
--- js/charts.js
+++ js/charts.js
@@ -1,44 +1,181 @@
 "use strict";
 
-const CHARTS = {
+(() => {
+
+function pieCallback(config) {
+  const data = google.visualization.arrayToDataTable(config.data);
+
+  if (!config.hasOwnProperty("colors")) {
+    config.colors = [
+        "#2196f3",  // blue-500
+        "#f44336",  // red-500
+        "#ff9800",  // orange-500
+        "#4caf50",  // green-500
+    ].slice(0, config.data.length - 1);
+  }
+  const slices = config.colors.map((color) => ({color: color}));
+
+  const chart = new google.visualization.PieChart(
+    document.getElementById(config.id)
+  );
+  chart.draw(data, {
+    pieHole: 0.4,
+    fontName: "'Raleway', 'Verdana', 'Arial', sans-serif",
+    pieSliceText: "percent",
+    fontSize: 14,
+    legend: {
+      position: "none",
+    },
+    tooltip: {
+      trigger: "none",
+    },
+    chartArea: {
+      top: 16,
+      left: 16,
+      bottom: 16,
+      right: 16,
+    },
+    slices: slices,
+    height: 320,
+    width: 320,
+  });
+}
+
+const i18n = {
+  sr: {
+    months: [
+      "januar", "februar", "mart", "april", "maj", "jun",
+      "jul", "avgust", "septembar", "oktobar", "novembar", "decembar",
+    ],
+  },
+};
+i18n.sr.shortMonths = i18n.sr.months.map((month) => month.substr(0, 3));
+
+function calendarCallback(config) {
+  const dataTable = new google.visualization.DataTable();
+  dataTable.addColumn({type: "date", id: "date" });
+  dataTable.addColumn({type: "number", id: "value" });
+  dataTable.addColumn({type: "string", role: "tooltip", p: {html: true}});
+  dataTable.addRows(makeRows(config.data));
+
+  const div = document.getElementById(config.id);
+  const chart = new google.visualization.Calendar(div);
+
+   google.visualization.events.addListener(chart, "ready", () => {
+     const textNodes = div.querySelectorAll("text");
+     // Remove legend: [low, mid, high] values.
+     textNodes[0].remove();
+     textNodes[1].remove();
+     textNodes[2].remove();
+     // Remove legend box and gradient:
+     div.querySelectorAll("[fill-opacity='1']").forEach((node) => {
+       node.remove();
+     });
+     div.querySelectorAll("linearGradient").forEach((node) => {
+       node.remove();
+     });
+     // Translate months:
+     i18n.sr.shortMonths.forEach((month, i) => {
+       textNodes[i + 4].innerHTML = month.toUpperCase();
+     });
+  });
+
+  chart.draw(dataTable, {
+    legend: "none",
+    calendar: {
+      cellSize: 16,
+      daysOfWeek: "NPUSČPS",
+      monthLabel: {
+        fontName: "'Raleway', 'Verdana', 'Arial', sans-serif",
+        fontSize: 14,
+      },
+    },
+    tooltip: {
+      isHtml: true,
+    },
+    height: 320,
+    width: 920,
+  });
+}
+
+function makeRows(data) {
+  const map = new Map();
+  data.forEach((row) => {
+    if (row[0].length === 1) {
+      map.set(row[0][0], [
+        row[0][0],
+        row[1],
+        makeTooltip(row[0][0], row[2]),
+      ]);
+    } else {
+      dateRange(row[0][0], row[0][1]).forEach((date) => map.set(date, [
+        date,
+        row[1],
+        makeTooltip(date, row[2]),
+      ]));
+    }
+  });
+
+  // IE does not support map.values().
+  const rows = [];
+  map.forEach((value) => rows.push(value));
+  rows.sort((a, b) => {
+    if (a[0].getTime() > b[0].getTime()) {
+      return 1;
+    }
+    if (a[0].getTime() < b[0].getTime()) {
+      return -1;
+    }
+    return 0;
+  });
+  return rows;
+}
+
+function dateRange(start, stop) {
+  const result = [new Date(start)];
+  while (start < stop) {
+    start.setDate(start.getDate() + 1);
+    result.push(new Date(start));
+  }
+  return result;
+}
+
+function makeTooltip(date, text) {
+  const div = document.createElement("div");
+  div.className = "tooltip";
+
+  const strong = document.createElement("strong");
+  strong.innerText = text;
+  div.appendChild(strong);
+
+  const br = document.createElement("br");
+  div.appendChild(br);
+
+  const em = document.createElement("em");
+  em.innerText = `${
+    date.getDate()
+  }. ${
+    i18n.sr.months[date.getMonth()]
+  } ${
+    date.getFullYear()
+  }.`;
+  div.appendChild(em);
+
+  return div.outerHTML;
+}
+
+window.CHARTS = {
   pie(config) {
     google.charts.setOnLoadCallback(() => {
-      const data = google.visualization.arrayToDataTable(config.data);
-
-      if (!config.hasOwnProperty("colors")) {
-        config.colors = [
-            "#2196f3",  // blue-500
-            "#f44336",  // red-500
-            "#ff9800",  // orange-500
-            "#4caf50",  // green-500
-        ].slice(0, config.data.length - 1);
-      }
-      const slices = config.colors.map((color) => ({color: color}));
-
-      const chart = new google.visualization.PieChart(
-        document.getElementById(config.id)
-      );
-      chart.draw(data, {
-        pieHole: 0.4,
-        fontName: "'Raleway', 'Verdana', 'Arial', sans-serif",
-        pieSliceText: "percent",
-        fontSize: 14,
-        legend: {
-          position: "none",
-        },
-        tooltip: {
-          trigger: "none",
-        },
-        chartArea: {
-          top: 16,
-          left: 16,
-          bottom: 16,
-          right: 16,
-        },
-        slices: slices,
-        height: 320,
-        width: 320,
-      });
+      pieCallback(config);
     });
-  }
+  },
+
+  calendar(config) {
+    google.charts.setOnLoadCallback(() => {
+      calendarCallback(config);
+    });
+  },
 };
+
+})();
